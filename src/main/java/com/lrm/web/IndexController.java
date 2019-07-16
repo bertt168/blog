48275d4ae809a264e6a3404171eff6fc50fbe502
shengpo <bertt168@gmail.com>
2019-07-16 13:33:52 +0800

    [why] 導入靜態頁面 [modified] indexController.java

--- src/main/java/com/lrm/web/IndexController.java
+++ src/main/java/com/lrm/web/IndexController.java
@@ -2,19 +2,12 @@ package com.lrm.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class IndexController {
 
-    @GetMapping("/{id}/{name}")
-    public String index(@PathVariable Integer id,@PathVariable String name) {
-//        int i = 9/0;
-//        String blog = null;
-//        if (blog == null) {
-//            throw new NotFoundException("blog 首頁 不存在");
-//        }
-        System.out.println("-----index-----");
+    @GetMapping("/")
+    public String index() {
         return "index";
     }
 }
